003781ef3be88b6e1ef05ee40b9953c6494a605b
Glabay <Java_Guru@live.com>
2024-11-30 12:16:56 -0400

    Addition: adds a website main class

---
+++ http-website/src/main/java/xyz/glabaystudios/Website.java
@@ -0,0 +1,18 @@
+package xyz.glabaystudios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Glabay | Glabay-Studios
+ * @project GlabTech
+ * @social Discord: Glabay
+ * @since 2024-11-30
+ */
+@SpringBootApplication
+public class Website {
+
+    public static void main(String... args) {
+        SpringApplication.run(Website.class, args);
+    }
+}
