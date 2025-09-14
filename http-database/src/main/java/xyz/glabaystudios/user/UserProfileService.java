bd92c035de5cd11c03030b0f59d2b1f6ef66a6a0
Glabay <Java_Guru@live.com>
2025-09-14 12:09:57 -0300

    Feat:
    Adds functionality to search for users and check if one exists
    
    Signed-off-by: Glabay <Java_Guru@live.com>

--- http-database/src/main/java/xyz/glabaystudios/user/UserProfileService.java
+++ http-database/src/main/java/xyz/glabaystudios/user/UserProfileService.java
@@ -1,8 +1,13 @@
 package xyz.glabaystudios.user;
 
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
 import xyz.glabaystudios.dto.UserProfileDto;
+import xyz.glabaystudios.inter.impl.UserProfileConverter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Glabay | Glabay-Studios
@@ -12,7 +17,7 @@ import xyz.glabaystudios.dto.UserProfileDto;
  */
 @Service
 @RequiredArgsConstructor
-public class UserProfileService {
+public class UserProfileService implements UserProfileConverter {
     private final UserProfileRepository playerProfileRepository;
 
     public UserProfile createNewPlayerProfile(UserProfileDto dto) {
@@ -23,7 +28,19 @@ public class UserProfileService {
             creation.setUpdatedAt(java.time.LocalDateTime.now());
         return playerProfileRepository.saveAndFlush(creation);
     }
+
     public UserProfile findByUsername(String username) {
         return playerProfileRepository.findByUsernameIgnoreCase(username).orElse(null);
+    }
+
+    public List<UserProfileDto> findAll() {
+        var profiles = playerProfileRepository.findAll();
+        return profiles.stream()
+            .map(this::mapToDto)
+            .collect(Collectors.toList());
+    }
+
+    public @Nullable Boolean userExists(String username) {
+        return playerProfileRepository.existsByUsernameIgnoreCase(username);
     }
 }
